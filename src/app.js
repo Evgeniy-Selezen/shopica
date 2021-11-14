4778242a88215b6df4c03be7600341b4fbcef870
holms-lm <sergbiletskii@gmail.com>
2021-11-14 15:58:45 +0200

    webpack styles

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import './scss/base.scss'
+import './icon.png';
+
+console.log('js init');
---
+++ src/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: { index: ['./app.js', './scss/base.scss'] },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                { from: "ignore/production" },
+            ],
+        })
+    ],
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].js',
+        clean: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
+                ],
+            },
+            // {
+            //     test: /\.css$/i,
+            //     use: ['style-loader', 'css-loader'],
+            // },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    }
+};
