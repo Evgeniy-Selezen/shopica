ca06496263d181fa8034f9cd89fa42226cecd46b
holms-lm <sergbiletskii@gmail.com>
2021-11-14 16:00:50 +0200

    webpack styles dev

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         new MiniCssExtractPlugin(),
         new CopyPlugin({
             patterns: [
-                { from: "ignore/production" },
+                { from: "ignore/develop" },
             ],
         })
     ],
